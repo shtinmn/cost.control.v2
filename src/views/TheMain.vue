344ca5b0e0bbd289335b67ff566529a0270918c8
Shtin Maksim <shtinmn@gmail.com>
2021-03-31 21:29:03 +0300

    feat: added lib vue-csv-import

--- src/views/TheMain.vue
+++ src/views/TheMain.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>the main</h1>
+  <div>
+    <vue-csv-import v-model="parseCsv" :map-fields="fields"></vue-csv-import>
+  </div>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+import { VueCsvImport } from 'vue-csv-import'
+
+@Component({
+  components: {
+    VueCsvImport,
+  },
+})
+export default class TheMain extends Vue {
+  parseCsv = null
+  fields = { field1: 'Label 1', field2: 'Label 2' }
+}
+</script>
