429e9a8568527c22cb9b1a59212097ac133a5c6b
Shtin Maksim <shtinmn@gmail.com>
2021-04-12 23:01:50 +0300

    feat: added footer

--- src/components/templates/AppTemplate.vue
+++ src/components/templates/AppTemplate.vue
@@ -3,5 +3,10 @@
     <v-main>
       <slot />
     </v-main>
+    <v-footer app tile padless width="100%">
+      <v-card flat tile width="100%" class="teal lighten-5 text-center">
+        <v-card-text class="black--text"> {{ new Date().getFullYear() }} </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
