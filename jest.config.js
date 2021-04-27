f3f26c8533680bd41d4f0abfc40ddc8bd483d703
Shtin Maksim <shtinmn@gmail.com>
2021-04-27 20:45:32 +0300

    feat: extends jest.conf & ts.conf & added js.conf

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    'vuetify/lib(.*)': '<rootDir>/node_modules/vuetify/es5$1',
+  },
+  modulePaths: ['<rootDir>/src', '<rootDir>/node_modules'],
+  transform: {
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.ts?$': 'ts-jest',
+    '.*\\.(vue)$': 'vue-jest',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!(vuetify)/)'],
+  setupFiles: ['jest-canvas-mock'],
 }
